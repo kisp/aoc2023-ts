864cec844c1a33dc0911c5f95e81b36ce6ede55f
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-12-09 15:44:07 +0100

    wip

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,33 +1,33 @@
-import input from "./input";
+import input from "./input"
 
 function firstDigit(line: string): string {
-  const match = line.match(/\d/);
+  const match = line.match(/\d/)
   if (!match) {
-    throw new Error("firstDigit: no digit found");
+    throw new Error("firstDigit: no digit found")
   }
-  return match[0];
+  return match[0]
 }
 
 function reverseString(s: string): string {
-  const arr = s.split("");
-  arr.reverse();
-  return arr.join("");
+  const arr = s.split("")
+  arr.reverse()
+  return arr.join("")
 }
 
 function lastDigit(line: string): string {
-  return firstDigit(reverseString(line));
+  return firstDigit(reverseString(line))
 }
 
 function lineValue(line: string): number {
-  return Number(firstDigit(line)) * 10 + Number(lastDigit(line));
+  return Number(firstDigit(line)) * 10 + Number(lastDigit(line))
 }
 
 function plus(a: number, b: number): number {
-  return a + b;
+  return a + b
 }
 
 function partA(): number {
-  return input.map(lineValue).reduce(plus);
+  return input.map(lineValue).reduce(plus)
 }
 
-console.log(partA());
+console.log(partA())
