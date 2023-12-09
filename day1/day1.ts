5d2bf8f432375158bf67af9f715d2c5417a3f2eb
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-12-09 15:41:25 +0100

    works

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,7 +1,33 @@
 import input from "./input";
 
-function foo() {
-  return input.length;
+function firstDigit(line: string): string {
+  const match = line.match(/\d/);
+  if (!match) {
+    throw new Error("firstDigit: no digit found");
+  }
+  return match[0];
 }
 
-console.log(foo());
+function reverseString(s: string): string {
+  const arr = s.split("");
+  arr.reverse();
+  return arr.join("");
+}
+
+function lastDigit(line: string): string {
+  return firstDigit(reverseString(line));
+}
+
+function lineValue(line: string): number {
+  return Number(firstDigit(line)) * 10 + Number(lastDigit(line));
+}
+
+function plus(a: number, b: number): number {
+  return a + b;
+}
+
+function partA(): number {
+  return input.map(lineValue).reduce(plus);
+}
+
+console.log(partA());
